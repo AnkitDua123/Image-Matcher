1ea642dbff95ce482dff129ecd61cd36983b72c4
AnkitDua123 <dua.ankit8@gmail.com>
2025-01-19 20:57:59 +0530

    Initial commit

--- Image-Matcher/src/main/java/com/ImageMatcherProject/Image_Matcher/Entity/Image.java
+++ Image-Matcher/src/main/java/com/ImageMatcherProject/Image_Matcher/Entity/Image.java
@@ -6,15 +6,54 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Image {
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public void setImageData(byte[] imageData) {
+        this.imageData = imageData;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public byte[] getImageData() {
+        return imageData;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
